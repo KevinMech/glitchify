a734c82c86109cd83cd93719c7efccca05845e60
Kevin <kevin.mellachew@gmail.com>
2017-07-31 17:13:04 -0400

    Add script execution functionality to background script

--- src/glitchify.js
+++ src/glitchify.js
@@ -1,3 +1,18 @@
+function onExecuted(result){
+    console.log('success!');
+}
+
+function onError(error){
+    throw error;
+}
+
+function glitchify(){
+    var script = browser.tabs.executeScript({
+        file: ""
+    });
+    script.then(onExecuted, onError);
+}
+
 (function(){
     browser.browserAction.onClicked.addListener(glitchify);
 })();
