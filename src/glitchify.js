ede0489d708a2de866ab235c9fa0854f0cf56108
Kevin <kevin.mellachew@gmail.com>
2017-08-10 23:57:41 -0400

    Add content script for changing background color

--- src/glitchify.js
+++ src/glitchify.js
@@ -1,18 +1,20 @@
-function onExecuted(result){
-    console.log('success!');
-}
-
-function onError(error){
-    throw error;
-}
-
-function glitchify(){
-    var script = browser.tabs.executeScript({
-        file: ""
-    });
-    script.then(onExecuted, onError);
-}
-
 (function(){
+
+    function onExecuted(result){
+        console.log('success!');
+    }
+
+    function onError(error){
+        throw error;
+    }
+
+    function glitchify(){
+        var script = browser.tabs.executeScript({
+            file: "content_scripts/background_change.js"
+        });
+        script.then(onExecuted, onError);
+    }
+
     browser.browserAction.onClicked.addListener(glitchify);
+    
 })();
